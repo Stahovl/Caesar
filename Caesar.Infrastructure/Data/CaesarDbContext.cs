5b0a877a27f112f7bcbf4e81abc827eabce2fa4f
Stahovl <vtahovs@gmail.com>
2024-09-26 23:19:16 +0500

    add doc folder, start documentation for Infrastucture

--- Caesar.Infrastructure/Data/CaesarDbContext.cs
+++ Caesar.Infrastructure/Data/CaesarDbContext.cs
@@ -3,13 +3,39 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Caesar.Infrastructure.Data;
 
+
+/// <summary>
+/// Represents the Entity Framework database context for the application.
+/// Сontext is responsible for creating the database tables, setting field validations,
+/// and seeding the initial admin user after the tables are created.
+/// </summary>
 public class CaesarDbContext : DbContext
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CaesarDbContext"/> class with the specified options.
+    /// </summary>
+    /// <param name="options">The options to be used by a <see cref="DbContext"/>.</param>
     public CaesarDbContext(DbContextOptions<CaesarDbContext> options) : base(options) { }
 
+    /// <summary>
+    /// Gets or sets the Reservations table.
+    /// </summary>
     public DbSet<Reservation> Reservations { get; set; }
+
+    /// <summary>
+    /// Gets or sets the MenuItems table.
+    /// </summary>
     public DbSet<MenuItem> MenuItems { get; set; }
+
+    /// <summary>
+    /// Gets or sets the Users table.
+    /// </summary>
     public DbSet<User> Users { get; set; }
+
+    /// <summary>
+    /// Configures the schema needed for the context.
+    /// </summary>
+    /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
